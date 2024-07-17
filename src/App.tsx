3f946f8a9c5437b416e291503ab3431a4a5a6fb4
tabularasa066a <tabularasa066a@gmail.com>
2024-07-17 22:41:47 +0900

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import logo from "./assets/react.svg";
 import "./App.css";
-import MyComponent from "./MyComponent";
+import UserForm from "./UserForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <MyComponent></MyComponent>
+        <UserForm></UserForm>
       </header>
     </div>
   );
---
+++ src/UserForm.tsx
@@ -0,0 +1,42 @@
+import "./UserForm.css"
+import { useState } from "react"
+
+const UserForm = () => {
+    const [name, setName] = useState("");
+    const [age, setAge] = useState(0)
+
+    const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value)
+    };
+
+    const onChangeAge = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const age = Number(e.target.value)
+        setAge(age)
+    }
+
+    const onSubmit = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+
+        const user = { id: Math.random().toString(), name, age }
+        console.log('submit: ', user)
+    }
+
+    return (
+        <div>
+            <div className="content-body">
+                <h1>Name is {name}</h1>
+                <form className="content-form">
+                    <label>Name:</label>
+                    <input type="text" onChange={(e) => onChangeName(e)} />
+                    
+                    <label>Age:</label>
+                    <input type="text" onChange={(e) => onChangeAge(e)} />
+                    
+                    <button onClick={(e) => onSubmit(e)}>Submit</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default UserForm;
